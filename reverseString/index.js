69b9e4a7329999b60ec0559c151224cae118dcb5
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-05-05 21:27:03 +0500

    Exercise 1: Reverse string solution 2

--- reverseString/index.js
+++ reverseString/index.js
@@ -6,13 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+/*Solution 1 */
+// function reverse(str) {
+//   const stringLength = str.length;
+//   const reverseStringArray = [];
+//   for ( let i = stringLength - 1; i >= 0; i-- ) {
+//     reverseStringArray.push(str[i]);
+//   }
+//   return reverseStringArray.join("");
+// }
+
+//Solution 2
 function reverse(str) {
-  const stringLength = str.length;
-  const reverseStringArray = [];
-  for ( let i = stringLength - 1; i >= 0; i-- ) {
-    reverseStringArray.push(str[i]);
-  }
-  return reverseStringArray.join("");
+  return str.split("").reverse().join("");
 }
 
 module.exports = reverse;
