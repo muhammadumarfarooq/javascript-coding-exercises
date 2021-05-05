b044d70869be96104b07b5d4b999327be204a85f
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-05-05 21:34:19 +0500

    Exercise 1: Reverse string solution 3

--- reverseString/index.js
+++ reverseString/index.js
@@ -17,8 +17,22 @@
 // }
 
 //Solution 2
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// Solution 3
 function reverse(str) {
-  return str.split("").reverse().join("");
+  let reversed = "";
+  // for(let i = 0; i < str.length; i++){
+  //   reversed = `${str[i]}${reversed}`
+  // }
+  //using for of loop
+  for ( let character of str ) {
+    reversed = `${character}${reversed}`;
+  }
+  
+  return reversed;
 }
 
 module.exports = reverse;
