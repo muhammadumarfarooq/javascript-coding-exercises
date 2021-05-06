dc46675d3fdcf33b5e3fa28cb40e99ca4921d2fb
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-05-06 21:43:57 +0500

    using debugger

--- reverseString/index.js
+++ reverseString/index.js
@@ -6,6 +6,14 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+//Steps to run debugger
+// run these command
+// node inspect index.js
+// cont
+// repl
+// then write the names of variables and inspect them.
+
+
 /*Solution 1 */
 // function reverse(str) {
 //   const stringLength = str.length;
@@ -30,9 +38,18 @@ function reverse(str) {
   //using for of loop
   for ( let character of str ) {
     reversed = `${character}${reversed}`;
+    debugger;
   }
   
   return reversed;
 }
+
+// Solution 3
+// function reverse(str) {
+//   debugger;
+//   return str.split("").reduce((prev, char) => `${char}${prev}`, "");
+// }
+//
+reverse("asdb");
 
 module.exports = reverse;
